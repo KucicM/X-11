a1ee1c68cd6ddb085c41fb8f37d9d425203e6b79
kucicm <10778553+KucicM@users.noreply.github.com>
2023-10-29 22:13:35 +0100

    clicking works

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
@@ -11,26 +14,43 @@ type SearchResult struct {
 }
 
 func main() {
+
+    port := flag.Int("port", 7323, "port")
+    knowledge_base_path := flag.String("path", "", "where are txt documents")
+    flag.Parse()
+
+    if *knowledge_base_path == "" {
+        log.Fatalln("path must be provided")
+    }
+
+
     http.Handle("/assets/", http.FileServer(http.Dir(".")))
+    http.HandleFunc("/", rootHander)
+    http.HandleFunc("/search", searchHandler)
 
-    http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-        // todo store in memory
-        tmpl := template.Must(template.ParseFiles("./templates/index.html"))
-        tmpl.Execute(w, nil)
-    })
+    log.Printf("server running on port %d\n", *port)
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
 
-    http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request){
-        query := r.URL.Query()
-        str := query.Get("query")
+func rootHander(w http.ResponseWriter, _ *http.Request) {
+    // todo store in memory
+    tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+    tmpl.Execute(w, nil)
+}
 
-        tmpl := template.Must(template.ParseFiles("./templates/search_results.html"))
-        data := map[string][]SearchResult{
-            "Results": {{"test1"}, {"test2"}, {"test3"}, {str}},
-        }
-        tmpl.Execute(w, data)
-    })
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+    query := r.URL.Query()
+    str := strings.TrimSpace(query.Get("query"))
 
-    log.Println("server running")
-    log.Fatal(http.ListenAndServe(":7323", nil))
+    if str == "" {
+        w.Write([]byte(""))
+        return
+    }
+
+    tmpl := template.Must(template.ParseFiles("./templates/search_results.html"))
+    data := map[string][]SearchResult{
+        "Results": {{"test1"}, {"test2"}, {"test3"}, {str}},
+    }
+    tmpl.Execute(w, data)
 }
 
