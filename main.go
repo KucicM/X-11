62f6acf53752f503461283a08042c0b84130c0c5
kucicm <10778553+KucicM@users.noreply.github.com>
2023-10-29 14:52:03 +0100

    dynamic results with mock data

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"text/template"
 )
 
+type SearchResult struct {
+    Title string
+}
+
 func main() {
     http.Handle("/assets/", http.FileServer(http.Dir(".")))
 
@@ -15,10 +19,15 @@ func main() {
         tmpl.Execute(w, nil)
     })
 
-    http.HandleFunc("/search", func(_ http.ResponseWriter, r *http.Request){
+    http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request){
         query := r.URL.Query()
         str := query.Get("query")
-        log.Printf("got %+s\n", str)
+
+        tmpl := template.Must(template.ParseFiles("./templates/search_results.html"))
+        data := map[string][]SearchResult{
+            "Results": {{"test1"}, {"test2"}, {"test3"}, {str}},
+        }
+        tmpl.Execute(w, data)
     })
 
     log.Println("server running")
