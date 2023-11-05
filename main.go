d18d5021325bdc1ef1579251b40692c159b24cac
kucicm <10778553+KucicM@users.noreply.github.com>
2023-11-05 23:24:52 +0100

    trie in seperate file and caching

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"strings"
 	"text/template"
-	"time"
 )
 
 type SuggestionResult struct {
@@ -22,13 +21,6 @@ type SearchResult struct {
 var trie *Trie
 
 func main() {
-    trie = NewTrie()
-    trie.build([]string{
-        "real test1",
-        "real test2",
-        "real test3",
-    })
-    
     port := flag.Int("port", 7323, "port")
     knowledge_base_path := flag.String("path", "", "where are txt documents")
     flag.Parse()
@@ -37,6 +29,12 @@ func main() {
         log.Fatalln("path must be provided")
     }
 
+    ch := make(chan string, 5)
+    for _, w := range []string{"real test1", "real test2", "real test3"} {
+        ch <- w
+    }
+    close(ch)
+    trie = BuildTrie(ch)
 
     http.Handle("/assets/", http.FileServer(http.Dir(".")))
     http.HandleFunc("/", rootHander)
@@ -53,7 +51,7 @@ func rootHander(w http.ResponseWriter, _ *http.Request) {
 }
 
 func suggestHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Cache-Control", "private, max-age=3600")
+    //w.Header().Add("Cache-Control", "private, max-age=3600")
     query := strings.TrimSpace(r.URL.Query().Get("query"))
     if query == "" {
         return
@@ -87,87 +85,3 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
     tmpl.Execute(w, data)
 }
 
-
-// trie for every prefix cache most common results
-type Trie struct {
-    root *TrieNode
-}
-
-func NewTrie() *Trie {
-    return &Trie{newTrieNode()}
-}
-
-func (t *Trie) build(words []string) {
-    log.Println("building trie")
-    defer func(started time.Time) {
-        log.Printf("trie build in %v\n", time.Since(started))
-    }(time.Now())
-    var node *TrieNode
-    for _, word := range words {
-        node = t.root
-        for _, b := range []byte(word) {
-            node = node.getNext(b)
-        }
-        node.mark()
-    }
-}
-
-func (t *Trie) finaAll(prefix string) []string {
-    node := t.root
-    // todo search should not create new nodes
-    for _, b := range []byte(prefix) {
-        node = node.getNext(b)
-    }
-
-    bRets := node.findAll()
-    ret := make([]string, 0, len(bRets))
-
-    bprefix := []byte(prefix)
-    for _, bs := range bRets {
-        buff := make([]byte, 0, len(bprefix) + len(bs))
-        buff = append(buff, bprefix...)
-
-        for i := len(bs)-1; i >= 0; i-- {
-            buff = append(buff, bs[i])
-        }
-        ret = append(ret, string(buff))
-    }
-    return ret
-}
-
-type TrieNode struct {
-    next map[byte]*TrieNode
-    cnt uint
-}
-
-func newTrieNode() *TrieNode {
-    return &TrieNode{make(map[byte]*TrieNode), 0}
-}
-
-func (t *TrieNode) getNext(b byte) *TrieNode {
-    if val, ok := t.next[b]; ok {
-        return val
-    }
-    val := newTrieNode()
-    t.next[b] = val
-    return val
-}
-
-func (t *TrieNode) mark() {
-    t.cnt += 1
-}
-
-func (t *TrieNode) findAll() [][]byte{
-    ret := make([][]byte, 0)
-    if t.cnt > 0 {
-        ret = append(ret, []byte(""))
-    }
-
-    for k, v := range t.next {
-        for _, val := range v.findAll() {
-            val = append(val, k)
-            ret = append(ret, val)
-        }
-    }
-    return ret
-}
---
+++ trie.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"sort"
+)
+
+type Trie struct {
+    root *trieNode
+}
+
+func BuildTrie(words chan string) *Trie {
+    trie := &Trie{root: newTrieNode()}
+    var node *trieNode
+
+    for word := range words {
+        node = trie.root
+        for _, w := range []byte(word) {
+            node = node.next_or_create(w)
+        }
+        node.markFinal(word)
+    }
+
+    trie.populateCache(10)
+    return trie
+}
+
+func LoadTrie(path string) (*Trie, error) {
+    return nil, nil
+}
+
+func (t *Trie) Save(path string) error {
+    return nil
+}
+
+func (t *Trie) populateCache(n int) {
+    t.root.populateCache(n)
+}
+
+func (t *Trie) finaAll(prefix string) []string {
+    node := t.root
+    for i := 0; i < len(prefix) && node != nil; i++ {
+        node = node.next(prefix[i])
+    }
+
+    if node == nil {
+        return []string{}
+    }
+    return node.topN()
+}
+
+type wordFreq struct {
+    freq uint
+    word string
+}
+
+type trieNode struct {
+    children map[byte]*trieNode
+    word wordFreq
+    topNWords []*wordFreq
+}
+
+func (n *trieNode) populateCache(topN int) []*wordFreq {
+    n.topNWords = make([]*wordFreq, 0, len(n.children))
+
+    if len(n.children) == 0 && n.word.freq == 0 {
+        return n.topNWords
+    }
+
+    n.topNWords = append(n.topNWords, &n.word)
+    if len(n.children) == 0 {
+        return n.topNWords
+    }
+
+    for _, child := range n.children {
+        n.topNWords = append(n.topNWords, child.populateCache(topN)...)
+    }
+
+    sort.Slice(n.topNWords, func(i, j int) bool {
+        return n.topNWords[i].freq > n.topNWords[j].freq
+    })
+
+    n.topNWords = n.topNWords[:min(topN, len(n.topNWords))]
+    return n.topNWords
+}
+
+func newTrieNode() *trieNode {
+    return &trieNode{children: make(map[byte]*trieNode), word: wordFreq{}}
+}
+
+func (n *trieNode) next_or_create(b byte) *trieNode {
+    if val, ok := n.children[b]; ok {
+        return val
+    }
+    val := newTrieNode()
+    n.children[b] = val
+    return val
+}
+
+func (n *trieNode) next(b byte) *trieNode {
+    return n.children[b]
+}
+
+func (n *trieNode) markFinal(word string) {
+    n.word.freq += 1
+    n.word.word = word
+}
+
+func (n *trieNode) topN() []string {
+    ret := make([]string, 0, len(n.topNWords))
+    for _, w := range n.topNWords {
+        ret = append(ret, w.word)
+    }
+    return ret
+}
